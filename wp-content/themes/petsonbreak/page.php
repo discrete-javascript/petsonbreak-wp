b1796ca5c9655ef58ed4240b57ffc6bf8f3754a8
Rajesh W <wraejesh@gmail.com>
2017-08-03 14:39:21 +0530

    Initial commit for Sliders

--- wp-content/themes/petsonbreak/page.php
+++ wp-content/themes/petsonbreak/page.php
@@ -274,6 +274,14 @@ else{
     </div>
 </div>
  <div id="all_categories">
+
+ <?php foreach (array_chunk($users_kicks, 4, true) as $array) {
+    echo '<div>';
+    foreach($array as $kicks) {
+         echo $kicks->brand;
+    }
+    echo '</div>'; ?>
+}
     <div class="row">
     <?php 
     $weekresults =$wpdb->get_results("select * from twc_petfriendly_destination where 1 ".$extQuery." ".$extrCond."");
@@ -299,6 +307,54 @@ else{
     <?php } ?>
     </div>
 </div>
+<div class="container">
+  <h2>Carousel Example</h2>  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <div class="row">
+            <div class="col-xs-3"><img src="la.jpg" alt="Los Angeles" style="width:100%;"></div>
+            <div class="col-xs-3"><img src="la.jpg" alt="Los Angeles" style="width:100%;"></div>
+            <div class="col-xs-3"><img src="la.jpg" alt="Los Angeles" style="width:100%;"></div>
+            <div class="col-xs-3"><img src="la.jpg" alt="Los Angeles" style="width:100%;"></div>
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="row">
+            <div class="col-xs-3"><img src="la.jpg" alt="Los Angeles" style="width:100%;"></div>
+            <div class="col-xs-3"><img src="la.jpg" alt="Los Angeles" style="width:100%;"></div>
+            <div class="col-xs-3"><img src="la.jpg" alt="Los Angeles" style="width:100%;"></div>
+            <div class="col-xs-3"><img src="la.jpg" alt="Los Angeles" style="width:100%;"></div>
+        </div>
+      </div>
+    
+      <div class="item">
+        <div class="row">
+            <div class="col-xs-3"><img src="la.jpg" alt="Los Angeles" style="width:100%;"></div>
+            <div class="col-xs-3"><img src="la.jpg" alt="Los Angeles" style="width:100%;"></div>
+            <div class="col-xs-3"><img src="la.jpg" alt="Los Angeles" style="width:100%;"></div>
+            <div class="col-xs-3"><img src="la.jpg" alt="Los Angeles" style="width:100%;"></div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
 <?php strong_testimonials_view( 1 ); ?>.
 <script>
     window.onload = function() { 
