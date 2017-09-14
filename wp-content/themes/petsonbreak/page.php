e691f8f4e8290a3b4f949e69a8113b0711027c26
DharminL <dharmin.loliyana@gmail.com>
2017-09-14 23:47:57 +0530

    Hotel API code added

--- wp-content/themes/petsonbreak/page.php
+++ wp-content/themes/petsonbreak/page.php
@@ -319,12 +319,182 @@ else{
 </div>
 <div class="about-us-section">
     <div class="about-description-container">
-        <div class="about-us-description">
+        <!-- <div class="about-us-description">
             <h1 class="about-us-heading">ABOUT US</h1>
             <p class="about-us-para-1">At PetsonBreak we mean what we say and are committed to providing superlative experiences that can rarely be found elsewhere. For us, PetsonBreak is more than just a travel company; it is the embodiment of everything that we are truly passionate about. PetsonBreak is owned and staffed by pet people for whom work related with pets and travel is not simply an interesting job, but an all-consuming passion. </p>
             <p class="about-us-para-2">We believe that travel should not simply be a business, but a way of exploring and understanding the diverse cultures, people and traditions that inhabit in this world and what more you can ask if your furry friend can tag along in these adventurous trips.</p>
+        </div> -->
+        <div class="about-us-description">
+          <div class="search_Form horizontal-box">
+         <div id="exTab1" >
+          <div class="div_clol-notuse pet-divclol">
+               <ul  class="nav nav-pills">
+              <li class="tabbss tab_1 active" ng-click="selectType(1);"><a  href="javascript:void(0)" data-toggle="tab" title="Hotel" ALT="Hotel"  return false;><i class="fa fa-bed" ></i><span><?php echo $mk_options['tab_hotel'];?></span></a></li>
+              <li class="tabbss tab_0"><a ng-click="selectType(0);" href="javascript:void(0)" data-toggle="tab" title="Flight" ALT="Flight"><i class="fa fa-plane"></i><span><?php echo $mk_options['tab_flight'];?></span></a></li>
+              <li class="tabbss tab_4" ng-click="selectType(4);"><a href="javascript:void(0)" data-toggle="tab" title="Vacation" ALT="Vacation"><i class="fa fa-umbrella"></i><span><?php echo $mk_options['tab_holiday'];?></span></a></li>
+              <li class="tabbss tab_3" ng-click="selectType(3)"><a href="javascript:void(0)" data-toggle="tab" title="Resort" ALT="Resort"><i class="fa fa-home"></i><span><span><?php echo $mk_options['tab_resort'];?></span></a></li>
+              <li class="tabbss tab_5" ng-click="selectType(5)"><a href="javascript:void(0)" data-toggle="tab" title="Bed & Breakfast" ALT="Bed & Breakfast"><i class="fa fa-coffee"></i><span><?php echo $mk_options['tab_bedbreakfast'];?></span></a></li>
+              </ul>
+              
+               <div class="tab-content clearfix">
+                   <div class="tab-pane active" id="tab-hotel">
+                  <div class="searchbox" style="position: relative;">
+                    <form method="get" name="search_form" class="hotel-search-box-form">
+                      <ul>
+                      <li>
+                        <input type="text" ng-model="desti" placeholder="City, Hotel or place name"  ng-change="getLocation_Hint($viewValue)"/>
+                        <input type="hidden" ng-model="lat"/>
+                        <input type="hidden" ng-model="lon"/>
+                        <input type="hidden" ng-model="regionid"/>
+                        <input type="hidden" ng-model="datatype"/>
+                        <input type="hidden" id="hotelType" name="hotelType" value="1"/>
+                        <i class="fa fa-map-marker popup" aria-hidden="true"></i>
+                        <div class="popup showhidepopup{{showpopup}}">
+                        <div class="show-autocomplete-popup"> 
+                          <div class="cities_15">
+                             <h6 >Cities</h6>
+                            <a class="autocomplete-dropdown" ng-repeat="city_destinations in city_destinations track by $index" ng-click="Update_Search_Field(city_destinations.lat, city_destinations.lon,city_destinations.id, city_destinations.latinFullName, 'location')">{{ city_destinations.latinFullName }} </a> 
+                          </div>
+                        
+                        <div class="hotels_15">
+                          <h6 class="hotels-2">Hotels</h6>
+                          <a class="autocomplete-dropdown" ng-repeat="hotel_destinations in hotel_destinations track by $index"  ng-click="Update_Search_Field(hotel_destinations.lat, hotel_destinations.lon,hotel_destinations.id, hotel_destinations.latinFullName, 'hotel')">{{ hotel_destinations.latinFullName }}</a>
+                        </div>
+                        <div class="landmarks_15">
+                           <h6 class="hotels-2">Landmarks</h6>
+                          <a class="autocomplete-dropdown" ng-repeat="landmarks_destinations in landmarks_destinations track by $index" ng-click="Update_Search_Field(landmarks_destinations.lat, city_destinations.lon, landmarks_destinations.id,landmarks_destinations.latinFullName, 'location')">{{ landmarks_destinations.latinFullName }}</a> 
+                        </div>
+                        </div>
+                        </div>
+                      </li>
+                      <li  class="input-daterange" data-date-format="M d, D">
+                        <div class="datepicker-background-div">
+                        <input type="text" placeholder="Check In" name="start" id="checkIn" readonly="true" />
+                        <i class="fa fa-calendar" aria-hidden="true"></i> </div>
+                        <div class="datepicker-background-div">
+                        <input type="text"placeholder="Check Out"  name="end" id="checkOut" readonly="true" />
+                        <i class="fa fa-calendar" aria-hidden="true"></i> </div>
+                      </li>
+                      <li>
+                        <div class="adults-12">
+                        <input type="text" name="Cri_Rooms" id="Cri_Rooms" rel="0" value="{{ Totaladults }} Adults in {{ rooms }} Room" readonly="readonly" style="cursor:pointer">
+                        <i class="fa fa-male" aria-hidden="true"></i> </div>
+                      </li>
+                      <li>
+                        <input class="btn-hack button" id="findbestrate" type="button" value="Sniff" ng-click="Search_Destinations();" >
+                      </li>
+                      </ul>
+                      <div class="children-age roomgroupdata hidnumberofrooms">
+                      <div class="room_text1" style="color:#000">
+                        <div class="nomar">
+                        <?php _e('Rooms','adivaha');?>
+                        </div>
+                        <div class="nomar">
+                        <?php _e('Adults','adivaha');?>
+                        </div>
+                        <div class=" nomar">
+                        <?php _e('Children','adivaha');?>
+                        </div>
+                      </div>
+                      <div class="how_noofRooms" >
+                        <div class="nomar add-rooms"> <a ng-click="changeRooms('minus');"><i class="fa fa-minus" aria-hidden="true"></i> </a> <a>{{count}}</a> <a ng-click="changeRooms('plus');"><i class="fa fa-plus" aria-hidden="true"></i> </a>
+                        <input type="hidden" class="form-control" name="Cri_noofRooms" id="Cri_noofRooms" value="{{count}}">
+                        </div>
+                        <div id="packListdiv" >&nbsp;</div>
+                      </div>
+                      <div class="be-ddn-footer"> <a href="javascript:void(0)" class="done" ng-click="hideRoomGroup();">Done</a> </div>
+                      </div>
+                    </form>
+                  </div>
+                 </div>
+                 
+                 <div class="tab-pane" id="tab-flight">
+                   <div class="searchbox flight-search-box" ng-controller="Flight_Controller">
+                  <form method="get" name="search_form" action="http://twc5.com/demo/tp/#/search/">
+                    <ul>
+                      <li>
+                      <input class="location_first1" type="text" ng-model="flight_desti" placeholder="{{Flights_City_From}}"  ng-change="getLocation_Hint_Flights_From($viewValue)"  />
+                      <input type="hidden" ng-model="flight_locationId" value="{{Flights_City_From_IATACODE}}" />
+                      <!--<i class="fa fa-map-marker popup" aria-hidden="true"></i>-->
+                      <div class="popup showhidepopup_flightsfrom{{showpopup_flightsfrom}} hidethisinitially locationpopup_flightsfrom">
+                        <div class="show-autocomplete-popup"> <a class="autocomplete-dropdown" ng-repeat="Flight_from_destis in Flight_from_destinations track by $index"> <span ng-if="Flight_from_destis.name" ng-click="Update_Search_Field_Flights_From(Flight_from_destis.city_code, Flight_from_destis.name)">{{ Flight_from_destis.name }}<span class="city_name_pup">{{ Flight_from_destis.city_name }}</span></span> <span ng-if="!Flight_from_destis.name" ng-click="Update_Search_Field_Flights_From(Flight_from_destis.city_code, Flight_from_destis.city_name)">{{ Flight_from_destis.city_name }}</span> </a> </div>
+                      </div>
+                      </li>
+                      <li>
+                      <input class="location_first2" type="text" ng-model="flight_to_desti" placeholder="{{Flights_City_To}}" value="{{Flights_City_To}}"  ng-change="getLocation_Hint_Flights_To($viewValue)"/>
+                      <input type="hidden" ng-model="flight_to_locationId" value="{{Flights_City_To_IATACODE}}" />
+                      <!--<i class="fa fa-map-marker popup" style="left: 15em;" aria-hidden="true"></i>-->
+                      <div class="popup showhidepopup_flightsto{{showpopup_flightsto}} hidethisinitially locationpopup_flightsto">
+                        <div class="show-autocomplete-popup flights_topop"> <a class="autocomplete-dropdown" ng-repeat="Flight_To_destis in Flight_To_destinations track by $index"> <span ng-if="Flight_To_destis.name"  ng-click="Update_Search_Field_Flights_To(Flight_To_destis.city_code ,Flight_To_destis.name )">{{ Flight_To_destis.name }} <span class="city_name_pup"> {{ Flight_To_destis.city_name }} </span></span> <span ng-if="!Flight_To_destis.name"  ng-click="Update_Search_Field_Flights_To(Flight_To_destis.city_code ,Flight_To_destis.city_name )">{{ Flight_To_destis.city_name }}</span> </a> </div>
+                      </div>
+                      </li>
+                      <li class="input-daterange" data-date-format="M d, D">
+                      <div class="datepicker-background-div"> <a href="javascript:void(0);" ng-click="Show_OneWay_Round(true)" class="direct{{directcri}}"> <i class="fa fa-long-arrow-right rxchange-room-3"></i> </a> <a href="javascript:void(0);" ng-click="Show_OneWay_Round(false)" class="return{{returncri}}"> <i class="fa fa-exchange rxchange-room-3"> </i> </a> </div>
+                      <div class="datepicker-background-div">
+                        <input type="text" placeholder="Arival" name="flight_to_start" id="flight_to_checkIn" date-format="mm/dd/yyyy" value="{{flight_to_checkIn}}" readOnly="true"/>
+                        <i class="fa fa-calendar" aria-hidden="true"></i> </div>
+                      <div class="datepicker-background-div">
+                        <input type="text"placeholder="Departure"  name="flight_to_end" id="flight_to_checkOut" ng-disabled="showhidecheckout" value="{{flight_to_checkOut}}" readOnly="true"/>
+                        <i class="fa fa-calendar" aria-hidden="true"></i> </div>
+                      </li>
+                      <li>
+                      <div ng-repeat="datum in data" class="width-set-room">
+                      <input class="passenger_buiness-1" type="text" id="showPassenger" ng-click="showPassenger();" rel="0" value="{{count+count1+count2}} passenger,{{result}}" readonly="readonly" style="cursor:pointer">
+                      <input type="hidden" ng-model="Flights_Adults" id="Flights_Adults" value="{{count}}" />
+                      <input type="hidden" ng-model="Flights_Children" value="{{count1}}" />
+                      <input type="hidden" ng-model="Flights_Infants" value="{{count2}}" />
+                      <input type="hidden" ng-model="Flights_Category_Economy" value="{{result}}" />
+                      <i class="fa fa-male" aria-hidden="true"></i> </li>
+                      <li>
+                      <input class="btn-hack button" type="button" value="Sniff" ng-click="Search_Flights();" >
+                      </li>
+                    </ul>
+                    <div class="children-age roomgroupdata hidnumberofrooms" >
+                      <div class="room-add-age" class="dropdown">
+                      <ul>
+                      <li>
+                        <label class="pax-title">Adults <span>Above 12 years</span></label>
+                        <div class="pules-min"> <a ng-click="count = count -1" > <i class="fa fa-minus" aria-hidden="true"></i> </a> <a>{{count}}</a> <a ng-click="count = count +1"> <i class="fa fa-plus" aria-hidden="true"></i> </a> </div>
+                      </li>
+                      <li>
+                        <label class="pax-title">Children <span>Below 12 years</span></label>
+                        <div class="pules-min"> <a ng-click="count1=count1 - 1" > <i class="fa fa-minus" aria-hidden="true"></i> </a> <a>{{count1}}</a> <a ng-click="count1=count1 +1"> <i class="fa fa-plus" aria-hidden="true"></i> </a> </div>
+                      </li>
+                      <li>
+                        <label class="pax-title">Infants <span>Below 2 years</span></label>
+                        <div class="pules-min"> <a ng-click="count2=count2 - 1"> <i class="fa fa-minus" aria-hidden="true"></i> </a> <a>{{count2}}</a> <a ng-click="count2= count2 +1"> <i class="fa fa-plus" aria-hidden="true"></i> </a> </div>
+                      </li>
+                      </ul>
+                      <div class="ddChild ddchild_ border shadow" id="flight_class_select_child">
+                      <ul>
+                        <li>
+                        <label>
+                          <input type="radio" ng-model="result" name="rdoResult" value="Economy" ng-checked="true">
+                          Economy</label>
+                        </li>
+                        <li>
+                        <label>
+                          <input type="radio" ng-model="result" name="rdoResult" value="Business">
+                          Business</label>
+                        </li>
+                      </ul>
+                      <div class="be-ddn-footer" modal="showModal"> <a href="javascript:void(0)" class="done" ng-click="done();">Done</a> </div>
+                      </div>
+                    </div>
+                    </div>
+                    </form>
+                   </div>
+                </div>
+                 
+               </div>
+          </div>
+         </div>
+      </div>
         </div>
     </div>
+  <div ui-view="search_results"></div>
+  <div ui-view="flight_search_View"></div>
+  <div ui-view></div>
         
     
 </div>
